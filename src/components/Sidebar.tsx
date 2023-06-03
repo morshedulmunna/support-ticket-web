e5be68cd1db1e54af08e3a3b82c7384155f25611
Morshedul Munna <morshedulmunna1@gmail.com>
2023-06-03 17:57:44 +0600

    Updated Ui

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,69 +1,54 @@
-"use client";
+'use client';
 
-import { Url } from "@/utils/basic";
-import { getSingleUser } from "@/utils/getUsers";
-import Tostify from "@/utils/Tostify";
-import axios from "axios";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
-import { toast } from "react-toastify";
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 
 const Sidebar = () => {
   const [user, setUser] = useState<any>({});
 
-  useEffect(() => {
-    getSingleUser(setUser);
-  }, []);
-
   return (
     <>
-      <Tostify>
-        <div className="mt-12 rounded bg-blue-50 px-3 py-6">
-          <Link
-            className="block w-full rounded-md py-2 px-4 hover:bg-blue-100"
-            href={"/support-center"}
-          >
-            {user?.foundUser?.roll === "admin" ? "Dashboard" : "Ticket History"}
-          </Link>
-          <Link
-            className="block w-full rounded-md py-2 px-4 hover:bg-blue-100"
-            href={"/support-center/open-tickets"}
-          >
-            Open Tickets
-          </Link>
-          <Link
-            className="block w-full rounded-md py-2 px-4 hover:bg-blue-100"
-            href={"/support-center/close-tickets"}
-          >
-            Close Tickets
-          </Link>
+      <div className="mt-12 rounded bg-blue-50 px-3 py-6">
+        <Link
+          className="block w-full rounded-md py-2 px-4 hover:bg-blue-100"
+          href={'/support-center'}>
+          {user?.foundUser?.roll === 'admin' ? 'Dashboard' : 'Ticket History'}
+        </Link>
+        <Link
+          className="block w-full rounded-md py-2 px-4 hover:bg-blue-100"
+          href={'/support-center/open-tickets'}>
+          Open Tickets
+        </Link>
+        <Link
+          className="block w-full rounded-md py-2 px-4 hover:bg-blue-100"
+          href={'/support-center/close-tickets'}>
+          Close Tickets
+        </Link>
 
-          {user?.foundUser?.roll === "admin" && (
-            <Link
-              className="block w-full rounded-md py-2 px-4 hover:bg-blue-100"
-              href={"/support-center/customer"}
-            >
-              Customers
-            </Link>
-          )}
-
+        {user?.foundUser?.roll === 'admin' && (
           <Link
             className="block w-full rounded-md py-2 px-4 hover:bg-blue-100"
-            href={"/support-center/settings"}
-          >
-            Settings
+            href={'/support-center/customer'}>
+            Customers
           </Link>
+        )}
 
-          {user?.foundUser?.roll !== "admin" && (
-            <Link href={"/support-center/create-ticket"}>
-              <button className=" mt-6 w-full rounded-md bg-blue-500 py-2  text-white hover:bg-blue-400 ">
-                Create an Ticket
-              </button>
-            </Link>
-          )}
-        </div>
-      </Tostify>
+        <Link
+          className="block w-full rounded-md py-2 px-4 hover:bg-blue-100"
+          href={'/support-center/settings'}>
+          Settings
+        </Link>
+
+        {user?.foundUser?.roll !== 'admin' && (
+          <Link href={'/support-center/create-ticket'}>
+            <button className=" mt-6 w-full rounded-md bg-blue-500 py-2  text-white hover:bg-blue-400 ">
+              Create an Ticket
+            </button>
+          </Link>
+        )}
+      </div>
     </>
   );
 };
