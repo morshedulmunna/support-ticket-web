92ea070d2aca202e017bf61902e250644fc7a613
morshedulmunna <morshedulmunna1@gmail.com>
2023-07-01 21:15:10 +0600

    added new ui

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,33 +1,33 @@
-import axios, { AxiosRequestConfig } from 'axios';
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { RegisterUser } from '@/types/custome-type';
+import { RegisterUser } from "@/types/custome-type";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const API = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SERVER_ADDRESS,
   withCredentials: false,
   timeout: 1000,
-  headers: { 'Access-Control-Allow-Origin': '*' },
+  headers: { "Access-Control-Allow-Origin": "*" },
 });
 
 API.interceptors.request.use((req) => {
-  const user = localStorage.getItem('user') || 'null';
+  const user = localStorage.getItem("user") || "null";
 
-  req.headers!['Authorization'] = `Bearer ${JSON.parse(user)?.token || ''}`;
+  req.headers!["Authorization"] = `Bearer ${JSON.parse(user)?.token || ""}`;
   return req;
 });
 export const useGetPost = () => {
-  return useQuery(['getPost'], async () => {
-    const response = await API.get<any>('/posts');
+  return useQuery(["getPost"], async () => {
+    const response = await API.get<any>("/posts");
     return response.data;
   });
 };
 
 // ============Register==============>
 export const userRegister = (body: RegisterUser) => {
-  return API.post('/auth/signup', body);
+  return API.post("/auth/signup", body);
 };
 
 // ============Login==============>
 export const userLogin = (body: any) => {
-  return API.post('/auth/signin', body);
+  return API.post("/auth/signin", body);
 };
